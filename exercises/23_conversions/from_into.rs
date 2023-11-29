cfa86123ca6be2a50c9b244edf3dc2e13f1a0956
Jake Allan <jjemail1995@gmail.com>
2023-11-29 18:31:59 +0000

    23_conversions

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -40,10 +40,33 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+
+        let mut parts = s.split(",");
+        let name = parts.next();
+        let age = parts.next();
+
+        match (name, age) {
+            (None, None) => Person::default(),
+            (None, Some(_)) => Person::default(),
+            (Some(_), None) => Person::default(),
+            (Some(name), Some(age)) => {
+                let name = String::from(name);
+
+                if (name == "") {
+                    return Person::default();
+                }
+
+                let age = match age.parse::<usize>() {
+                    Ok(age) => return Person { name, age },
+                    Err(e) => return Person::default(),
+                };
+            }
+        }
     }
 }
 
